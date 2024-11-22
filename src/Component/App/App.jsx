0c494f473332aa630ffbaef07534b8b79c800cb6
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2024-11-22 10:06:16 +0000

    Make use of outlet in routes

--- src/Component/App/App.jsx
+++ src/Component/App/App.jsx
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom";
-// import styles from './App.module.css'
+import { Link, Outlet } from "react-router-dom";
+import styles from './App.module.css'
 
 function App() {
   return (
     <>
-      <nav>
+      <nav className={styles.navBar}>
         <ul>
           <li>
-            <Link to="/home">Home</Link></li>
-            <li>
-            <Link to="/shop">Shop</Link></li>
-            <li>
-            <Link to="/cart">Cart</Link></li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shop">Shop</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
         </ul>
       </nav>
+      <Outlet />
     </>
   );
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,28 +2,28 @@ import App from "./Component/App/App";
 import HomePage from "./Component/HomePage/HomePage";
 import ShopPage from "./Component/ShopPage/ShopPage";
 import CartPage from "./Component/CartPage/CartPage";
-import ErrorPage from "./ErrorPage/ErrorPage";
+import ErrorPage from "./Component/ErrorPage/ErrorPage";
 
 const routes = [
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "/shop",
+        element: <ShopPage />,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+    ],
   },
-  {
-    path: "/home",
-    element: <HomePage />,
-  },
-  {
-    path: "/shop",
-    element: <ShopPage />,
-  },
-  {
-    path: "/cart",
-    element: <CartPage />,
-  },
-
 ];
 
-
-export default routes
+export default routes;
