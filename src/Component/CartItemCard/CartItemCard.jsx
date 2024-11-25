56a5ee6e116b307d014a7e055fb0a8d2628de947
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2024-11-25 14:34:22 +0000

    Add cart item card component

---
+++ src/Component/CartItemCard/CartItemCard.jsx
@@ -0,0 +1,18 @@
+import styles from "./CartItemCard.module.css"
+
+function CartItemCard({ imgURL, title, price, quantity }) {
+  return (
+    <div className={styles.cartItemCard}>
+      <img src={imgURL} alt="" />
+      <div>
+        <p>{title}</p>
+        <p>Price: {`$${price}`}</p>
+        <p>Quantity: {quantity}</p>
+        <p>Total: {price * quantity}</p>
+      </div>
+      <button type="button">Remove</button>
+    </div>
+  );
+}
+
+export default CartItemCard
