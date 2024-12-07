78acce6a62005eb9a7f7e43b107415ffb4c2a26d
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2024-12-07 12:58:08 +0000

    Add tests for error page

---
+++ src/Component/ErrorPage/ErrorPage.test.jsx
@@ -0,0 +1,37 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import ErrorPage from "./ErrorPage";
+import { BrowserRouter } from "react-router-dom";
+
+describe("ErrorPage component", () => {
+  it("check whether the error page elements are rendered", () => {
+    render(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>
+    );
+
+    const errorHeader = screen.getByRole("heading");
+    const homePageLink = screen.getByRole("link")
+    
+
+    expect(errorHeader).toHaveTextContent("Oh no, this route doesn't exist!");
+    expect(homePageLink).toHaveTextContent("You can go back to the home page by clicking here, though!");
+    expect(homePageLink).toHaveAttribute("href", "/");
+  });
+
+
+  it("check whether the error page link point to the right page", () => {
+    render(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>
+    );
+
+    const homePageLink = screen.getByRole("link")
+
+    expect(homePageLink).toHaveAttribute("href", "/");
+  });
+});
+
+
