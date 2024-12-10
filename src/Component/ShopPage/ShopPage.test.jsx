b7037d5822b35cbe87de342ccb4b52769613099d
JyrexRonaldo <65960417+JyrexRonaldo@users.noreply.github.com>
2024-12-10 11:50:13 +0000

    Modify tests for shop page component

--- src/Component/ShopPage/ShopPage.test.jsx
+++ src/Component/ShopPage/ShopPage.test.jsx
@@ -3,26 +3,27 @@ import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
+vi.mock("../ProductCard/ProductCard", () => {
+  return {
+    default: ({ imgURL, title, price, cardId }) => (
+      <div data-testid="productCard">
+        {JSON.stringify({ imgURL, title, price, cardId })}
+      </div>
+    ),
+  };
+});
+
 vi.mock(import("react-router-dom"), async (importOriginal) => {
   const actual = await importOriginal();
   return {
     ...actual,
-    // your mocked methods
-
     useOutletContext: () => ({
       productsData: [
         {
           id: 1,
           title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
           price: 109.95,
-          description:
-            "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-          category: "men's clothing",
           image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-          rating: {
-            rate: 3.9,
-            count: 120,
-          },
         },
       ],
       error: false,
@@ -32,7 +33,14 @@ vi.mock(import("react-router-dom"), async (importOriginal) => {
 });
 
 describe("ShopPage component", () => {
-  it.only("displays element", () => {
+  it.only("displays child components", () => {
+    const mockProductData = {
+      imgURL: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      price: 109.95,
+      cardId: 1,
+    };
+
     render(
       <BrowserRouter>
         <ShopPage />
@@ -41,9 +49,9 @@ describe("ShopPage component", () => {
 
     screen.debug();
 
-    expect(screen.getByRole("presentation")).toHaveAttribute("src", "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" )
+    console.log(screen.getByTestId("productCard").textContent);
 
-
-
+    expect(screen.getByTestId("productCard").textContent).toEqual(
+      JSON.stringify(mockProductData));
   });
 });
